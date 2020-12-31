7266c126d97928295f42d0e8636a242c626e2728
Boris Krasnoselskiy <boris.krasnoselskiy@digitalsuits.co>
2020-12-31 13:24:38 +0200

    Added stats plugin

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom'
 import React, { useRef, useMemo, useState } from 'react'
 import { Canvas, useFrame } from 'react-three-fiber'
 import { EffectComposer, SSAO } from 'react-postprocessing'
+import FPSStats from "react-fps-stats"
 import './styles.css'
 
 function Swarm({ count, mouse }) {
@@ -56,19 +57,23 @@ function Swarm({ count, mouse }) {
 
 function App() {
   return (
-    <Canvas
-      shadowMap
-      gl={{ alpha: false, antialias: false }}
-      camera={{ fov: 75, position: [0, 0, 70], near: 10, far: 150 }}
-      onCreated={(state) => state.gl.setClearColor('#f0f0f0')}>
-      <ambientLight intensity={1.5} />
-      <pointLight position={[100, 100, 100]} intensity={2} castShadow />
-      <pointLight position={[-100, -100, -100]} intensity={5} color="red" />
-      <Swarm count={150} />
-      <EffectComposer multisampling={0}>
-        <SSAO samples={31} radius={20} intensity={40} luminanceInfluence={0.1} color="black" />
-      </EffectComposer>
-    </Canvas>
+    <>
+      <FPSStats />
+
+      <Canvas
+        shadowMap
+        gl={{ alpha: false, antialias: false }}
+        camera={{ fov: 75, position: [0, 0, 70], near: 10, far: 150 }}
+        onCreated={(state) => state.gl.setClearColor('#f0f0f0')}>
+        <ambientLight intensity={1.5} />
+        <pointLight position={[100, 100, 100]} intensity={2} castShadow />
+        <pointLight position={[-100, -100, -100]} intensity={5} color="red" />
+        <Swarm count={150} />
+        <EffectComposer multisampling={0}>
+          <SSAO samples={31} radius={20} intensity={40} luminanceInfluence={0.1} color="black" />
+        </EffectComposer>
+      </Canvas>
+    </>
   )
 }
 
